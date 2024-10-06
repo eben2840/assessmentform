2719a7c83cedf8e92ac7c6713976054b308836a0
Ebenezer Aggrey-Mills <106875822+eben2840@users.noreply.github.com>
2024-10-06 20:23:08 +0000

    Newpip

--- forms.py
+++ forms.py
@@ -7,11 +7,11 @@ class AssessmentForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     product = SelectField('tag', choices=[('Tag','Tag'),('Very Satisfied', 'Very Satisfied'), ('Satisfied','Satisfied'), ('Neutral','Neutral'), ('Unsatisfied','Unsatisfied'), ('Very Unsatisfied','Very Unsatisfied') ], default=None )
     aplication = SelectField('tag', choices=[('Tag','Tag'),('Exceeded expectations', 'Exceeded expectations'), ('Met expectations','Met expectations'), ('Fell short of expectations','Fell short of expectations') ], default=None )
-    happy = SelectField('tag', choices=[('Tag','Tag'),('Very Satisfied', 'Very Satisfied'), ('Satisfied','Satisfied'), ('Neutral','Neutral'), ('Unsatisfied','Unsatisfied'), ('Very Unsatisfied','Very Unsatisfied') ], default=None )
+    happy = SelectField('tag', choices=[('Tag','Tag'),('Web/App Developement', 'Web/App Developement'), ('Graphic Design','Graphic Design'), ('Digital Marketing','Digital Marketing'), ('All The Above','All The Above')], default=None )
     overall = SelectField('tag', choices=[('Tag','Tag'),('Very Satisfied', 'Very Satisfied'), ('Satisfied','Satisfied'), ('Neutral','Neutral'), ('Unsatisfied','Unsatisfied'), ('Very Unsatisfied','Very Unsatisfied') ], default=None )
     time = SelectField('tag', choices=[('Tag','Tag'),('Yes', 'Yes'), ('No','No'), ('Needed More Time','Needed More Time')], default=None )
     project = SelectField('tag', choices=[('Tag','Tag'),('Very Satisfied', 'Very Satisfied'), ('Satisfied','Satisfied'), ('Neutral','Neutral'), ('Unsatisfied','Unsatisfied'), ('Very Unsatisfied','Very Unsatisfied') ], default=None )
-    services = SelectField('tag', choices=[('Tag','Tag'),('Yes', 'Yes'), ('No','No'), ('Needed More Time','Needed More Time')], default=None )
+    services = SelectField('tag', choices=[('Tag','Tag'),('Yes', 'Yes'), ('No','No'), ('Maybe', 'Maybe')], default=None )
     caption = StringField('caption', validators=[DataRequired()])
     
    
